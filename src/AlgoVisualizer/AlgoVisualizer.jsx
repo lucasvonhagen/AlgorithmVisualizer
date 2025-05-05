00be797dd301310c5ac45d72a4911da2d3707a64
lucasvonhagen <lucas.d.hansson@hotmail.com>
2025-05-05 13:45:36 +0200

    Added mergeSort

--- src/AlgoVisualizer/AlgoVisualizer.jsx
+++ src/AlgoVisualizer/AlgoVisualizer.jsx
@@ -15,13 +15,22 @@ export default class AlgoVisualizer extends React.Component{
     
     resetArray(){
         const array = [];
-        const maxBars = 100;
+        const maxBars = 700;
         const minBars = 10;
-        for (let i = 0; i < maxBars; i++){
+        for (let i = 0; i < 310; i++){
             array.push(randomIntFromInterval(minBars, maxBars));
         }
         this.setState({array});
     }
+
+
+    mergeSort(){
+       const jsSortedArray = this.state.array
+       .slice()
+       .sort((a, b) => a - b);
+    }
+    bubbleSort(){}
+
 
 
 
@@ -30,20 +39,30 @@ export default class AlgoVisualizer extends React.Component{
         const {array} = this.state;
 
         return (
-            <>
-            {array.map((value, idx) => (
-                <div className="array-Container" key={idx}>
-                     {value}
-                </div>
-            ))}
-            <button onClick={() => this.resetArray()}>Generate New Array</button>
-            </>
+            <div className='arrayCotainer'>
+                {array.map((value,idx) => (
+                    <div
+                        className='arrayBar'
+                        key={idx}
+                        style={{height: `${value}px`}}
+                    ></div>
+                ))}
+                <button onClick={() => this.resetArray()}>Generate New Array</button>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            </div>
         )
     }
 
 };
 
-
+function equalArrays(arrayOne, arrayTwo){
+    if (arrayOne.length !== arrayTwo.length) return false;
+    for(let i = 0; i<arrayOne.length; i++){
+        if(arrayOne[i] !== arrayTwo[i]) return false;
+    }
+    return true;
+}
 
 function randomIntFromInterval(min, max){
     return Math.floor(Math.random() * (max-min+1) + min);
